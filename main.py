15d844849c9a34922644b3286a8ab4266b202fe0
danielyml <74169623+danielyml@users.noreply.github.com>
2024-08-26 08:36:56 +0000

    26AUG2024

--- main.py
+++ main.py
@@ -37,3 +37,35 @@ print(x)
 # is the same as
 x = 'John'
 print(x)
+myvar = "John"
+my_var = "John"
+_my_var = "John"
+myVar = "John"
+MYVAR = "John"
+myvar2 = "John"
+x, y, z = "Orange", "Banana", "Cherry"
+print(x)
+print(y)
+print(z)
+x = y = z = "Orange"
+print(x)
+print(y)
+print(z)
+fruits = ["apple", "banana", "cherry"]
+x, y, z = fruits
+print(x)
+print(y)
+print(z)
+x = "Python is awesome"
+print(x)
+x = "Python"
+y = "is"
+z = "awesome"
+print(x, y, z)
+x = "Python "
+y = "is "
+z = "awesome"
+print(x + y + z)
+x = 5
+y = "John"
+print(x, y)
