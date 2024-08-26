64f5a7b351b33056f2a91287c0ed8504ac64f172
danielyml <74169623+danielyml@users.noreply.github.com>
2024-08-26 06:54:02 +0000

    26AUG2024

---
+++ main.py
@@ -0,0 +1,39 @@
+
+print("Hello, World!")
+import sys
+
+print(sys.version)
+if 5 > 2:
+  print("Five is greater than two!")
+#  if 5 > 2:      Due to inconsistency , it is to return error
+#   print("Five is greater than two!")
+#   print("Five is greater than two!")
+ 
+print("Hello, World!") #This is a comment
+#print("Hello, World!")
+print("Cheers, Mate!") 
+""" 
+Hello
+"""
+x = 5
+y = "John"
+print(x)
+print(y)
+x = 4       # x is of type int
+x = "Sally" # x is now of type str
+print(x)
+x = str(3)    # x will be '3'
+y = int(3)    # y will be 3
+z = float(3)
+print(x)
+print(y)
+print(z)
+x = 5
+y = "John"
+print(type(x))
+print(type(y))
+x = "John"
+print(x)
+# is the same as
+x = 'John'
+print(x)
